4d3f8ca77bcf47389f8054555b2714814eb1964c
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-05-05 16:00:22 -0300

    Atualização exercício 12

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,2 +1,23 @@
-var agora = new Date()
-var hora = agora.getHours()
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 6 && hora < 12){
+        msg.innerHTML += ` <br> Boa Dia!!`
+        img.src = 'fotos/manha.png'
+        document.body.style.background = '#e2cd9f'
+
+    } else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML += ` <br> Boa tarde!!`
+        img.src = 'fotos/tarde.png'
+        document.body.style.background = '#b9846f'
+
+    } else {
+        msg.innerHTML += ` <br> Boa Noite!!`
+        img.src = 'fotos/noite.png'
+        document.body.style.background = '#515154'
+
+}       
+}
