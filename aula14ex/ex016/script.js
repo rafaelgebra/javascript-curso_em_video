4f89ae17a1d72cfed2a1373fec543bda897db4c9
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-05-08 17:06:18 -0300

    Criação dos ex016 e ex017

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+var inicio = window.document.getElementById('txtinicio')
+var fim = window.document.getElementById('txtfim')
+var passo = window.document.querySelector('div#txtpasso')
+var c = 0
+var res = .innerHT
+function contar(){
+    for (c=1; inicio <= fim; c++ )
+        console.log(c)
+}
