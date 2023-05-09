c4f4f472a845f2566cea0f8d0fa73a515707ae03
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-05-09 13:07:39 -0300

    Atualização ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
+function gerar(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número valido!')
 
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <=10){
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
