0d252c3f9577ad9ecd643340e97f1b684fbcfbcd
Rafael Rodrigues Gebra <111754401+rafaelgebra@users.noreply.github.com>
2023-05-17 10:50:35 -0300

    theory update

---
+++ aula16ex/ex07/script.js
@@ -0,0 +1,33 @@
+
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    }else{
+        return false
+    }
+}
+
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.valeu, valores)) {
+        window.alert('Tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
+
+
